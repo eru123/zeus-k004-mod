eebe8df5d7d44f775b263aa0c6097c43a75b2d11
eru123 <yeoligoakino@gmail.com>
2022-02-27 18:19:42 +0800

    fix cmd not functioning

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from pynput import keyboard
 from pynput.keyboard import Key, Controller
 
 control = Controller()
-invalid_keys = [Key.scroll_lock]
+invalid_keys = [Key.scroll_lock, Key.cmd]
 
 def valid(key):
 	for invalid_key in invalid_keys:
@@ -25,5 +25,5 @@ def on_release(key):
 		return False
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-	print("Listening for keyboard input...")
+	print("Tested only on Zeus k004\n\nListening for keyboard input...")
 	listener.join()
