766b96c048001b14cef2a182be7d1adfc86ed9f0
eru123 <yeoligoakino@gmail.com>
2022-02-28 00:02:53 +0800

    change algo to on_release only

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import time
 from pynput import keyboard
 from pynput.keyboard import Key, Controller
 
 control = Controller()
-invalid_keys = [Key.scroll_lock, Key.cmd]
+invalid_keys = [Key.scroll_lock]
+light_duration = 0.01
 
 def valid(key):
 	for invalid_key in invalid_keys:
@@ -13,17 +15,15 @@ def valid(key):
 def toggle_light():
 	control.press(Key.scroll_lock)
 	control.release(Key.scroll_lock)
-
-def on_press(key):
-	if(not valid(key)): return
-	toggle_light()
 	
 def on_release(key):
 	if(not valid(key)): return
 	toggle_light()
+	time.sleep(light_duration)
+	toggle_light()
 	if key == Key.esc:
 		return False
 
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+with keyboard.Listener(on_release=on_release) as listener:
 	print("Tested only on Zeus k004\n\nListening for keyboard input...")
 	listener.join()
